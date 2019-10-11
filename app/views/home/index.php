20cc3c1fa0e211d35a2301efd4ae0a535b8fafd4
Rezza Mochamad <rezzamuhammad12@gmail.com>
2019-10-11 07:33:45 +0700

    Mengupload database

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -5,7 +5,7 @@
   <p class="lead">Halo, nama saya <?= $data['nama']; ?></p>
   <hr class="my-4">
   <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+  <a class="btn btn-primary btn-lg" href="https://about.me/rezza" role="button">Learn more</a>
 </div>
 
 </div>
